cc1b9e32c55cf346a8b888e05723c36466e21a2b
Marius048 <grandthaft5@gmx.de>
2025-08-03 01:37:14 +0200

    Update angebote.js

--- angebote.js
+++ angebote.js
@@ -1,1 +1,19 @@
+fetch("angebote.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const container = document.getElementById("angebote");
+    data.entries.forEach((angebot) => {
+      const el = document.createElement("div");
+      el.innerHTML = `
+        <div class="angebot">
+          <h3>${angebot.titel}</h3>
+          <img src="${angebot.bild}" alt="${angebot.titel}" />
+          <p>${angebot.details}</p>
+          <p><strong>Preis:</strong> ${angebot.preis}</p>
+          ${angebot.mehr ? `<a href="${angebot.mehr}">Mehr erfahren</a>` : ""}
+        </div>
+      `;
+      container.appendChild(el);
+    });
+  });
 
